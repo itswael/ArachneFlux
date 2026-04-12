b4bd5782bc91371b24fe444da2254aa08d30f1db
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2026-04-11 22:54:17 -0400

    created a simple scraper using jsoup

--- src/main/java/com/itswael/Main.java
+++ src/main/java/com/itswael/Main.java
@@ -3,11 +3,24 @@ package com.itswael;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+        log(doc.title());
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            log("%s\n\t%s",
+                    headline.attr("title"), headline.absUrl("href"));
+        }
+    }
 
+    private static void log(String msg, String... vals) {
+        System.out.printf((msg) + "%n", (Object[]) vals);
     }
 }
