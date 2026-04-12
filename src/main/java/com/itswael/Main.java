0716e51e16d0239bf1b1d6fd7ce1dcdb0476daf1
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2026-04-11 22:46:50 -0400

    Initial commit

---
+++ src/main/java/com/itswael/Main.java
@@ -0,0 +1,13 @@
+package com.itswael;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
