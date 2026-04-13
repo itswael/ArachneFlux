6bea613bbfa0204105044405494eca812317ac7f
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2026-04-12 21:45:34 -0400

    feat: added a domain class

---
+++ src/main/java/com/itswael/Domain.java
@@ -0,0 +1,19 @@
+package com.itswael;
+
+public class Domain {
+    private String domainHash;
+    private String domainUrl;
+
+    public Domain(String domainHash, String domainUrl) {
+        this.domainHash = domainHash;
+        this.domainUrl = domainUrl;
+    }
+
+    public String getDomainHash() {
+        return domainHash;
+    }
+
+    public String getDomainUrl() {
+        return domainUrl;
+    }
+}
